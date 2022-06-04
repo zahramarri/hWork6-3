d5cb183fc120296072c1171f3a681faef6d88524
zahramarri <zahramarri.dev@gmail.com>
2022-06-04 15:45:49 +0430

    Initial commit

---
+++ app/src/main/java/com/learning/hwork6_3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.learning.hwork6_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
